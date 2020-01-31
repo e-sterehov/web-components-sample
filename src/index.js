ddefa1cb8f8785048c7d4577c14f018b3c476fef
e-stereh <e.sterehov@gmail.com>
2020-01-31 16:12:49 -0500

    Implement babel

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 document.addEventListener('DOMContentLoaded', event => {
     const test = 'App is Running?';
-    console.log(test);
+    console.log(event);
+    test2(() => {
+        return 'test';
+    });
+    function test2(text) {
+        console.log(text);
+    }
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = ({ mode }) => {
@@ -30,6 +30,20 @@ module.exports = ({ mode }) => {
                 }
             ])
         ],
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['@babel/preset-env']
+                        }
+                    }
+                }
+            ]
+        },
         devtool: mode === 'development' ? 'source-map' : 'nonde'
     };
 };
